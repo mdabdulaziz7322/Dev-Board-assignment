1ab12495ce2907643aba56ef69862b1b90a745d3
Mdabdulaziz <mdabdulaziz7322@gmail.com>
2025-03-03 05:00:13 +0100

    clear button  updated

--- script.js/button.js
+++ script.js/button.js
@@ -8,6 +8,7 @@ let formattedTime = currentTime.toLocaleTimeString();
 
 for (const button of buttons) {
     button.addEventListener('click', function () {
+        console
         alert('Board Updated Successfully');
         convertedTask = convertedTask - 1;
         document.getElementById('remaining-tasks').innerText = convertedTask;
@@ -18,7 +19,9 @@ for (const button of buttons) {
         if (convertedTask == 0) {
             alert('congrats!!! You have completed all the current task');
         }
-        const text = document.getElementsByClassName('text').innerText;
+        // const text = document.getElementsByClassName('text').innerText;
+        const text =  document.getElementById('text-6').innerText;
+        console.log(text);
         const activityLog = document.getElementById('activity-log');
         const p = document.createElement("p");
         p.classList.add('p-style');
@@ -28,6 +31,10 @@ for (const button of buttons) {
         console.log(p);
 
         activityLog.appendChild(p);
+
+        document.getElementById('clear-btn').addEventListener('click', function(){
+            activityLog.removeChild(p);
+        })
         
 
 
