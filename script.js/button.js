405d5b610241504d038ef94421d5ce29bbee9de2
Mdabdulaziz <mdabdulaziz7322@gmail.com>
2025-03-03 02:47:48 +0100

    java script code for buttons updated

---
+++ script.js/button.js
@@ -0,0 +1,28 @@
+const buttons = document.getElementsByClassName('complete-btn');
+const remainingTask = document.getElementById('remaining-tasks').innerText;
+let convertedTask = parseInt(remainingTask);
+const taskFinished = document.getElementById('finished-tasks').innerText;
+let convertedFinishTasks = parseInt(taskFinished);
+let currentTime = new Date();
+let formattedTime = currentTime.toLocaleTimeString();
+
+for (const button of buttons) {
+    button.addEventListener('click', function () {
+        alert('Board Updated Successfully');
+        convertedTask = convertedTask - 1;
+        document.getElementById('remaining-tasks').innerText = convertedTask;
+        convertedFinishTasks = convertedFinishTasks + 1;
+        document.getElementById('finished-tasks').innerText = convertedFinishTasks;
+        this.disabled = true;
+        this.style.opacity = "0.5";
+        if (convertedTask == 0) {
+            alert('congrats!!! You have completed all the current task');
+        }
+        
+
+
+
+
+    })
+
+}
